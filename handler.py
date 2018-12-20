8fa2dbd90b38ebc900ad22ca23fe3126c7e23c38
Michael Meyer <meyermt@uchicago.edu>
2018-12-19 21:54:46 -0600

    start boto3 code

--- handler.py
+++ handler.py
@@ -1,7 +1,19 @@
 import json
-
+import os
+import boto3
 
 def hello(event, context):
+
+    ecr_repo = os.environ['ECR_REPO']
+    ecr = boto3.client('ecr')
+    s3 = boto3.client('s3')
+    # change below to download_file
+    s3_response = s3.get_response (
+        Bucket = os.environ['DOCKERFILE_BUCKET'],
+        Key = f'(os.environ['DEPLOY_ENV'])/Dockerfile' 
+    )
+    # dockerfile = 
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
